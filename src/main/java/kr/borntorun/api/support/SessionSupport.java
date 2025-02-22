6d1df7f368970589c7f37aed92f1fc3e5554592c
small-goliath <meang617@gmail.com>
2025-02-22 11:50:53 +0900

    oauth2 oidc 인증 시퀀스 다이어그램

--- src/main/java/kr/borntorun/api/support/SessionSupport.java
+++ src/main/java/kr/borntorun/api/support/SessionSupport.java
@@ -1,15 +1,14 @@
 package kr.borntorun.api.support;
 
-import org.springframework.http.HttpHeaders;
-
 import jakarta.servlet.http.HttpServletRequest;
+import kr.borntorun.api.domain.constant.TokenType;
 
 public class HeaderSupport {
 
 	private final static String TOKEN_PREFIX = "Bearer ";
 
 	public static String getAccessToken(HttpServletRequest request) {
-		String headerValue = request.getHeader(HttpHeaders.AUTHORIZATION);
+		String headerValue = String.valueOf(request.getSession().getAttribute(TokenType.ACCESS_TOKEN.name()));
 
 		if (headerValue == null) {
 			return null;

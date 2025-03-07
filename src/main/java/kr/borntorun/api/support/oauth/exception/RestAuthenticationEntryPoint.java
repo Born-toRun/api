b7a161affe252d48238d42c3bbe19a01a357edb6
small-goliath <meang617@gmail.com>
2025-03-07 09:37:50 +0900

    logging 보완

--- src/main/java/kr/borntorun/api/support/oauth/exception/RestAuthenticationEntryPoint.java
+++ src/main/java/kr/borntorun/api/support/oauth/exception/RestAuthenticationEntryPoint.java
@@ -17,7 +17,7 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	  HttpServletResponse response,
 	  AuthenticationException authException
 	) throws IOException {
-		log.info("Responding with unauthorized error. Message := {}", authException.getMessage());
+		log.info("[{} {}] Responding with unauthorized error: {}", request.getMethod().toUpperCase(), request.getRequestURI(), authException.getMessage());
 		response.sendError(
 		  HttpServletResponse.SC_UNAUTHORIZED,
 		  authException.getLocalizedMessage()
